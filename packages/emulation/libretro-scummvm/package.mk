f2e60ed58fa8fa8828a6871a5d994755fd8667e9
CvH <namerp@googlemail.com>
2019-01-22 15:01:08 +0100

    game.libretro.scummvm: fixes

--- packages/emulation/libretro-scummvm/package.mk
+++ packages/emulation/libretro-scummvm/package.mk
@@ -2,24 +2,25 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-scummvm"
-PKG_VERSION="20d71cd7189ae7fdd453a3041f3103bffabea13e"
-PKG_SHA256="571710ba831604096e1e0c76f764cf8e037adefd72a9b94d302bd51c6e94010d"
+PKG_VERSION="d6fb141ed00c70fd84cf78386e88c244265a14f1"
+PKG_SHA256="1f37fa2458be8a161848506246dcaf39fc587ee1fd786c50cfb69a3524c22ba4"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/scummvm"
 PKG_URL="https://github.com/libretro/scummvm/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain kodi-platform"
 PKG_LONGDESC="game.libretro.scummvm: scummvm for Kodi"
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="scummvm_libretro.so"
 PKG_LIBPATH="backends/platform/libretro/build/$PKG_LIBNAME"
 PKG_LIBVAR="SCUMMVM_LIB"
 
-make_target() {
+PKG_MAKE_OPTS_TARGET="-C backends/platform/libretro/build/"
+
+pre_make_target() {
   cd $PKG_BUILD
-  CXXFLAGS="$CXXFLAGS -DHAVE_POSIX_MEMALIGN=1"
-  export AR="$AR cru"
-  make -C backends/platform/libretro/build/
+  CXXFLAGS+=" -DHAVE_POSIX_MEMALIGN=1"
+  export AR+=" cru"
 }
 
 makeinstall_target() {
