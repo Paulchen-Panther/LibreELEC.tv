0e50a629583c7ecc012856c981a2988b861cac12
CvH <namerp@googlemail.com>
2019-03-10 10:23:40 +0100

    iw: update to 5.0.1

--- packages/addons/addon-depends/network-tools-depends/iw/package.mk
+++ packages/addons/addon-depends/network-tools-depends/iw/package.mk
@@ -2,14 +2,13 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="iw"
-PKG_VERSION="4.14"
-PKG_SHA256="f01671c0074bfdec082a884057edba1b9efd35c89eda554638496f03b769ad89"
-PKG_LICENSE="PUBLIC_DOMAIN"
+PKG_VERSION="5.0.1"
+PKG_SHA256="1e38ea794a223525b2ea7fe78fd14f2a56121e62e21ba5f9dbe8c494b35b5c0d"
+PKG_LICENSE="GPL"
 PKG_SITE="http://wireless.kernel.org/en/users/Documentation/iw"
-PKG_URL="https://www.kernel.org/pub/software/network/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://www.kernel.org/pub/software/network/iw/iw-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain libnl"
 PKG_LONGDESC="A new nl80211 based CLI configuration utility for wireless devices."
-# iw fails at runtime with lto enabled
 
 pre_configure_target() {
   export LDFLAGS="$LDFLAGS -pthread"
