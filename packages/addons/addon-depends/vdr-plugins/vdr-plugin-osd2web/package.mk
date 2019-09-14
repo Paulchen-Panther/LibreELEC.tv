f887ba1f7b7d319331808640f8ffebded66e723e
CvH <namerp@googlemail.com>
2019-09-15 01:22:47 +0200

    vdr-addon: add osd2web

--- packages/addons/addon-depends/vdr-plugins/vdr-plugin-osd2web/package.mk
+++ packages/addons/addon-depends/vdr-plugins/vdr-plugin-osd2web/package.mk
@@ -7,13 +7,14 @@ PKG_SHA256="d4b0593b37160c3cefe7ec3076d05208616d72673e4036f2b7d21278ee6f8d1e"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://github.com/horchi/vdr-plugin-osd2web"
 PKG_URL="https://github.com/horchi/vdr-plugin-osd2web/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain jansson libexif libwebsockets tinyxml2 vdr"
+PKG_DEPENDS_TARGET="toolchain jansson libexif libwebsockets2 vdr-tinyxml vdr"
 PKG_NEED_UNPACK="$(get_pkg_directory vdr)"
 PKG_LONGDESC="Allows a comfortable operation of VDR trough a web interface."
 PKG_BUILD_FLAGS="-gold"
 
 pre_configure_target() {
-  CFLAGS+="-I${SYSROOT_PREFIX}/usr/include -I$(get_build_dir jansson)/.$TARGET_NAME/include"
+#  CFLAGS+="-I${SYSROOT_PREFIX}/usr/include -I$(get_build_dir jansson)/.$TARGET_NAME/include -I$(get_build_dir libwebsockets2)/.$TARGET_NAME/include"
+  CFLAGS+="-I$(get_build_dir jansson)/.$TARGET_NAME/include -I$(get_build_dir libwebsockets2)/.$TARGET_NAME/include"
 }
 
 pre_make_target() {
--- packages/addons/service/vdr-addon/package.mk
+++ packages/addons/service/vdr-addon/package.mk
@@ -11,7 +11,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="https://libreelec.tv"
 PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain vdr vdr-plugin-ddci2 vdr-plugin-dummydevice vdr-plugin-dvbapi vdr-plugin-eepg vdr-plugin-epgfixer \
-                    vdr-plugin-epgsearch vdr-plugin-iptv vdr-plugin-restfulapi vdr-plugin-robotv vdr-plugin-satip \
+                    vdr-plugin-epgsearch vdr-plugin-iptv vdr-plugin-osd2web vdr-plugin-restfulapi vdr-plugin-robotv vdr-plugin-satip \
                     vdr-plugin-streamdev vdr-plugin-vnsiserver vdr-plugin-wirbelscan vdr-plugin-wirbelscancontrol vdr-plugin-xmltv2vdr"
 PKG_SECTION="service.multimedia"
 PKG_SHORTDESC="VDR: a TV streaming server for Linux"
@@ -25,10 +25,9 @@ PKG_ADDON_REQUIRES="pvr.vdr.vnsi:0.0.0 script.config.vdr:0.0.0"
 
 addon() {
   # create dirs
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/{bin,lib,plugin}
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config/epgsources
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/{eepg,epgfixer,epgsearch,streamdev-server,vnsiserver}
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/plugin
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/{eepg,epgfixer,epgsearch,osd2web,streamdev-server,vnsiserver}
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/res/plugins/restfulapi
 
   # configs
@@ -37,7 +36,6 @@ addon() {
 
   cp -P $(get_build_dir vdr)/{diseqc.conf,keymacros.conf,scr.conf,sources.conf,svdrphosts.conf} $ADDON_BUILD/$PKG_ADDON_ID/config
   cp -P $(get_build_dir vdr-plugin-epgfixer)/epgfixer/*.conf $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/epgfixer
-  cp -P $(get_build_dir vdr-plugin-osd2web)/*.conf $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/osd2web
   cp -P $(get_build_dir vdr-plugin-streamdev)/streamdev-server/streamdevhosts.conf $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/streamdev-server
   cp -P $(get_build_dir vdr-plugin-vnsiserver)/vnsiserver/allowed_hosts.conf $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/vnsiserver
 
@@ -49,7 +47,7 @@ addon() {
   cp -P $(get_build_dir vdr-plugin-xmltv2vdr)/dist/epgdata2xmltv/epgdata2xmltv.dist $ADDON_BUILD/$PKG_ADDON_ID/config/epgsources/epgdata2xmltv
 
   # copy binaries
-  for pkg in ddci2 dummydevice dvbapi eepg epgfixer epgsearch iptv restfulapi robotv satip vnsiserver wirbelscan wirbelscancontrol xmltv2vdr; do
+  for pkg in ddci2 dummydevice dvbapi eepg epgfixer epgsearch iptv osd2web restfulapi robotv satip vnsiserver wirbelscan wirbelscancontrol xmltv2vdr; do
     cp -PR $(get_build_dir vdr-plugin-$pkg)/libvdr*.so.* $ADDON_BUILD/$PKG_ADDON_ID/plugin
   done
 
@@ -60,4 +58,13 @@ addon() {
   cp -P $(get_build_dir vdr)/vdr $ADDON_BUILD/$PKG_ADDON_ID/bin/vdr.bin
 
   cp -P $(get_build_dir vdr-plugin-xmltv2vdr)/dist/epgdata2xmltv/epgdata2xmltv $ADDON_BUILD/$PKG_ADDON_ID/bin
+
+  # osd2web
+  cp -R $(get_build_dir vdr-plugin-osd2web)/.install_pkg/config/http $ADDON_BUILD/$PKG_ADDON_ID/config/plugins/osd2web
+
+  cp -L $(get_build_dir jansson)/.install_pkg/usr/lib/libjansson.so.4 \
+        $(get_build_dir libexif)/.install_pkg/usr/lib/libexif.so.12 \
+        $(get_build_dir libwebsockets2)/.$TARGET_NAME/lib/libwebsockets.so.12 \
+        $(get_build_dir vdr-tinyxml)/.install_pkg/usr/lib/libtinyxml2.so.7 \
+        $ADDON_BUILD/$PKG_ADDON_ID/lib
 }
