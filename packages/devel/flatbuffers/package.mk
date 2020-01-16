099e5c18dab7a594572d33c60ae4f844f5671f0e
Jonas Karlman <jonas@kwiboo.se>
2020-01-16 07:41:15 +0000

    flatbuffers: build and install lib, install flatc in sysroot

--- packages/devel/flatbuffers/package.mk
+++ packages/devel/flatbuffers/package.mk
@@ -6,34 +6,43 @@ PKG_VERSION="1.12.0"
 PKG_SHA256="62f2223fb9181d1d6338451375628975775f7522185266cd5296571ac152bc45"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/google/flatbuffers"
-PKG_URL="https://github.com/google/flatbuffers/archive/v$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/google/flatbuffers/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="toolchain:host"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain flatbuffers:host"
 PKG_LONGDESC="An efficient cross platform serialization library for games and other memory constrained apps."
 
-PKG_CMAKE_OPTS_HOST="-DFLATBUFFERS_CODE_COVERAGE=OFF \
-                     -DFLATBUFFERS_BUILD_TESTS=OFF \
-                     -DFLATBUFFERS_INSTALL=ON \
-                     -DFLATBUFFERS_BUILD_FLATLIB=OFF \
-                     -DFLATBUFFERS_BUILD_FLATC=ON \
+PKG_CMAKE_OPTS_HOST="-DFLATBUFFERS_BUILD_FLATC=ON \
                      -DFLATBUFFERS_BUILD_FLATHASH=OFF \
+                     -DFLATBUFFERS_BUILD_FLATLIB=OFF \
                      -DFLATBUFFERS_BUILD_GRPCTEST=OFF \
-                     -DFLATBUFFERS_BUILD_SHAREDLIB=OFF"
+                     -DFLATBUFFERS_BUILD_SHAREDLIB=OFF \
+                     -DFLATBUFFERS_BUILD_TESTS=OFF \
+                     -DFLATBUFFERS_CODE_COVERAGE=OFF \
+                     -DFLATBUFFERS_INSTALL=ON"
 
-PKG_CMAKE_OPTS_TARGET="-DFLATBUFFERS_CODE_COVERAGE=OFF \
-                       -DFLATBUFFERS_BUILD_TESTS=OFF \
-                       -DFLATBUFFERS_INSTALL=ON \
-                       -DFLATBUFFERS_BUILD_FLATLIB=OFF \
-                       -DFLATBUFFERS_BUILD_FLATC=OFF \
+PKG_CMAKE_OPTS_TARGET="-DFLATBUFFERS_BUILD_FLATC=ON \
                        -DFLATBUFFERS_BUILD_FLATHASH=OFF \
+                       -DFLATBUFFERS_BUILD_FLATLIB=ON \
                        -DFLATBUFFERS_BUILD_GRPCTEST=OFF \
-                       -DFLATBUFFERS_BUILD_SHAREDLIB=OFF"
+                       -DFLATBUFFERS_BUILD_SHAREDLIB=OFF \
+                       -DFLATBUFFERS_BUILD_TESTS=OFF \
+                       -DFLATBUFFERS_CODE_COVERAGE=OFF \
+                       -DFLATBUFFERS_INSTALL=ON"
 
 pre_configure_host() {
-  export CXXFLAGS="$CXXFLAGS -std=c++11"
+  export CXXFLAGS="${CXXFLAGS} -std=c++11"
 }
 
 post_makeinstall_host() {
-  mkdir -p $TOOLCHAIN/bin
-    cp -PR $PKG_BUILD/.$HOST_NAME/flatc $TOOLCHAIN/bin
+  mkdir -p ${TOOLCHAIN}/bin
+    cp -PR ${PKG_BUILD}/.${HOST_NAME}/flatc ${TOOLCHAIN}/bin
+}
+
+post_makeinstall_target() {
+  rm -rf ${INSTALL}/usr/bin
+
+  # HACK: we have flatc in $TOOLCHAIN/bin but it seems
+  # the one from sysroot prefix is picked when building hyperion. replace it!
+  rm -f ${SYSROOT_PREFIX}/usr/bin/flatc
+  cp -PR ${TOOLCHAIN}/bin/flatc ${SYSROOT_PREFIX}/usr/bin
 }
