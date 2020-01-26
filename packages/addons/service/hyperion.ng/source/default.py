cde34d31f250ca5e29a3aade1d7d373ce7c6ef0f
CvH <namerp@googlemail.com>
2020-01-26 16:09:44 +0100

    if you change something at the settings then the addon gets restarted

--- packages/addons/service/hyperion.ng/source/default.py
+++ packages/addons/service/hyperion.ng/source/default.py
@@ -1,7 +1,20 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
+import subprocess
 import xbmc
+import xbmcaddon
 
-monitor = xbmc.Monitor()
-monitor.waitForAbort()
+
+class Monitor(xbmc.Monitor):
+
+    def __init__(self, *args, **kwargs):
+        xbmc.Monitor.__init__(self)
+        self.id = xbmcaddon.Addon().getAddonInfo('id')
+
+    def onSettingsChanged(self):
+        subprocess.call(['systemctl', 'restart', self.id])
+
+
+if __name__ == '__main__':
+    Monitor().waitForAbort()
