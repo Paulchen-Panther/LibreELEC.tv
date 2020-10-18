e9aed41a3c52b46b121488eb054d396b2f73dd44
CvH <namerp@googlemail.com>
2020-10-18 12:25:20 +0200

    broken: hyperion-ng: initial package

---
+++ packages/addons/addon-depends/xcb-util/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="xcb-util"
+PKG_VERSION="0.4.0"
+PKG_SHA256="0ed0934e2ef4ddff53fcc70fc64fb16fe766cd41ee00330312e20a985fd927a7"
+PKG_LICENSE="MIT"
+PKG_SITE="https://xcb.freedesktop.org/XcbUtil"
+PKG_URL="https://xcb.freedesktop.org/dist/xcb-util-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libxcb"
+PKG_LONGDESC="The xcb-util module provides a number of libraries which sit on top of libxcb."
--- packages/addons/service/hyperion-ng/package.mk
+++ packages/addons/service/hyperion-ng/package.mk
@@ -2,16 +2,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="hyperion-ng"
-PKG_VERSION="1aba51e85c4c3f7cac438aa7bf6a845ef536b42e"
-PKG_SHA256="882dc3fa46a2edf1fd066649239dd27a8901077071a2418b5fd4d36eadf50fb0"
-PKG_VERSION_DATE="2020-01-15"
+PKG_VERSION="a0311bd8b58dd12aec0bdd62e1ec47adcaa340f4"
+PKG_SHA256="108c21e235243329e725a5dbf2a3ed79f026f1b3731f2c6c9e2685a8bb289550"
+PKG_VERSION_DATE="2020-11-17"
 PKG_REV="100"
-PKG_ARCH="any"
+PKG_ARCH="x86_64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/hyperion-project/hyperion.ng"
 PKG_URL="https://github.com/hyperion-project/hyperion.ng/archive/${PKG_VERSION}.tar.gz"
-PKG_DEPENDS_TARGET="toolchain Python3 libusb qtbase qtserialport protobuf rpi_ws281x avahi flatbuffers"
-PKG_DEPENDS_UNPACK="rpi_ws281x"
+PKG_DEPENDS_TARGET="toolchain Python3 libusb qtbase protobuf avahi flatbuffers flatbuffers:host libcec qtserialport"
 PKG_SECTION="service"
 PKG_SHORTDESC="Hyperion.NG: an AmbiLight controller"
 PKG_LONGDESC="Hyperion.NG (${PKG_VERSION_DATE}) is a modern opensource AmbiLight implementation."
@@ -21,46 +20,40 @@ PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="Hyperion.NG"
 PKG_ADDON_TYPE="xbmc.service"
 
-PKG_DISPMANX_SUPPORT="-DENABLE_DISPMANX=OFF"
-PKG_FB_SUPPORT="-DENABLE_FB=ON"
-PKG_X11_SUPPORT="-DENABLE_X11=OFF"
-
-if [ "${KODIPLAYER_DRIVER}" = "bcm2835-driver" ]; then
-  PKG_DEPENDS_TARGET+=" bcm2835-driver"
-  PKG_DISPMANX_SUPPORT="-DENABLE_DISPMANX=ON"
-  PKG_FB_SUPPORT="-DENABLE_FB=OFF"
-elif [ "${DISPLAYSERVER}" = "x11" ]; then
-  PKG_DEPENDS_TARGET+=" xorg-server xrandr"
-  PKG_X11_SUPPORT="-DENABLE_X11=ON"
+if [ "${DISPLAYSERVER}" = "x11" ]; then
+  PKG_DEPENDS_TARGET+=" xorg-server xrandr xcb-util"
+  PKG_X11_SUPPORT="-DENABLE_X11=ON -DENABLE_FB=OFF -DPLATFORM=x86"
 fi
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON \
+pre_configure_target() {
+  # workaround to define variable for patch
+  # src/hyperiond/hyperiond.cpp
+  export ENABLE_LE=1
+}
+
+PKG_CMAKE_OPTS_TARGET="-DPython3_EXECUTABLE=${TOOLCHAIN}/bin/${PKG_PYTHON_VERSION} \
+                       -DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON \
+                       -DCMAKE_BUILD_TYPE=Release \
                        -DENABLE_AMLOGIC=OFF \
-                       ${PKG_DISPMANX_SUPPORT} \
-                       ${PKG_FB_SUPPORT} \
                        -DENABLE_OSX=OFF \
                        -DUSE_SYSTEM_PROTO_LIBS=ON \
                        -DUSE_SYSTEM_FLATBUFFERS_LIBS=ON \
-                       -DENABLE_SPIDEV=ON \
+                       -DENABLE_SPIDEV=OFF \
                        -DENABLE_TINKERFORGE=OFF \
                        -DENABLE_V4L2=ON \
-                       -DENABLE_WS281XPWM=ON \
+                       -DENABLE_WS281XPWM=OFF \
                        ${PKG_X11_SUPPORT} \
                        -DENABLE_TESTS=OFF \
                        -Wno-dev \
                        -DQT_BIN_PATH=${TOOLCHAIN}/bin"
-
-pre_build_target() {
-  cp -a $(get_build_dir rpi_ws281x)/* ${PKG_BUILD}/dependencies/external/rpi_ws281x
-}
-
-pre_configure_target() {
-  echo "set(HYPERION_BUILD_ID \"${PKG_VERSION}\")" > ../cmake/FindGitVersion.cmake
-}
 
 addon() {
   mkdir -p $ADDON_BUILD/${PKG_ADDON_ID}/bin
     cp ${PKG_INSTALL}/usr/bin/* ${ADDON_BUILD}/${PKG_ADDON_ID}/bin
 
   cp -PR ${PKG_INSTALL}/usr/share/hyperion/effects ${ADDON_BUILD}/${PKG_ADDON_ID}
+
+  mkdir -p "$ADDON_BUILD/$PKG_ADDON_ID/lib"
+    cp "$(get_build_dir avahi)/avahi-compat-libdns_sd/.libs/libdns_sd.so.1" \
+       "$ADDON_BUILD/$PKG_ADDON_ID/lib"
 }
